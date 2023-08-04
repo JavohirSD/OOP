186536a89bf72e19d7884f5ff91c826a351d9c5c
Javohir Abdirasulov <54473854+JavohirSD@users.noreply.github.com>
2023-08-04 14:12:35 +0500

    Update Dependency-inversion.php

--- SOLID/Dependency-inversion.php
+++ SOLID/Dependency-inversion.php
@@ -4,7 +4,7 @@
  * Yuqori darajadagi modullar quyi darajadagi modullarga bog'liq bo'lmasligi kerak.
  * Ikkala turdagi modullar ham abstraktsiyalarga(interfeyslarga) bog'liq bo'lishi kerak.
  * Ya'ni klasslarda metod argumenti yoki xususiyati sifatida obyektlardan emas interfeyslardan foydalanish to'g'ri bo'ladi.
- * Prinsip yana ham tushunarli bo'lishi uchun quyidagi ma'lumotlar bazasi va UserDB moduli misolini ko'rib chiqamiz.
+ * Prinsip yana ham tushunarli bo'lishi uchun quyidagi ma'lumotlar bazasi va UserDB klass misolini ko'rib chiqamiz.
  */
 
 // Dependency Inversion ga zid bo'lgan holat:
@@ -14,9 +14,9 @@ class UserDB
     private $dbConnection;
 
     // Agar qachondir tizim MySQL dan PostgreSQL ga o'tkazilsa
-    // UserDB modulni qayta yozib chiqishga to'g'ri keladi (Bu esa OCP prinsipiga ham zid).
-    // Chunki bu tizim abstraksiyaga emas aynan bitta klass ga qaram bo'lib qolgan.
-    public function __construct(MySQLConnection $dbConnection)
+    // UserDB klassni qayta yozib chiqishga to'g'ri keladi (Bu esa OCP prinsipiga ham zid).
+    // Chunki bu tizim abstraksiyaga emas aynan bitta klass obyektiga qaram bo'lib qolgan.
+    public function __construct($dbConnection)
     {
         $this->$dbConnection = $dbConnection;
     }
@@ -34,8 +34,8 @@ interface DBConnectionInterface {
     public function connect();
 }
 
-// Istalgan vaqtda istalgan ma'lumotlar bazasi uchun yangi modul yozishimiz mumkun.
-// Lekin ushbu quyi modullar ham asbtraksiyaga bog'liq bo'lishi kerak.
+// Istalgan vaqtda istalgan ma'lumotlar bazasi uchun yangi klass yozishimiz mumkun.
+// Lekin ushbu quyi klasslar ham asbtraksiyaga bog'liq bo'lishi kerak.
 class MySQLConnection implements DBConnectionInterface {
     public function connect() {
         // Return the MySQL connection...
@@ -46,8 +46,8 @@ class UserDB {
 
     private $dbConnection;
 
-    // 1. Bu yerda quyi modullar yuqori(UserDB) modulga umuman bog'liq emas.
-    // 2. Ammo ikkala modul ham bitta abstraksiya(interfeys) ga bog'liq.
+    // 1. Bu yerda quyi klasslar yuqori(UserDB) klassga umuman bog'liq emas.
+    // 2. Ammo ikkala klass ham bitta abstraksiya(interfeys) ga bog'liq.
     // Sodda qilib aytganda argument sifatida biror bir klass obyektidan emas uning interfeysida foydalandik.
     // Demak tizim Dependency inversion prinsipi talabiga to'liq javob beradi.
     public function __construct(DBConnectionInterface $dbConnection) {
